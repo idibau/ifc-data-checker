a6504c875d9c633b20e393b7bc3aaadb71e4b10c
Dominik Fringeli <dfringeli@outlook.com>
2020-09-01 16:07:14 +0200

    added manifest in to include yml and schema json files

--- setup.py
+++ setup.py
@@ -15,6 +15,8 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/idibau/ifc-data-checker",
     packages=setuptools.find_packages(),
+    py_modules=["checker"],
+    install_requires=['pyyaml', 'jsonschema'],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
