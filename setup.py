9a5b907b809d664c04ea400a3fd9e46f3cd25b9a
Dominik Fringeli <dfringeli@outlook.com>
2020-09-01 15:21:21 +0200

    added setup.py

---
+++ setup.py
@@ -0,0 +1,25 @@
+"""setup"""
+
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="ifc-data-checker-dfringeli",  # Replace with your own username
+    version="0.0.1",
+    author="Dominik Fringeli",
+    author_email="dominik.fringeli@students.fhnw.ch",
+    description="The IFC Data Checker can validate rules on an ifc model.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/idibau/ifc-data-checker",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Environment :: Console"
+    ],
+    python_requires='>=3.8',
+)
