f600d494db2693cf5fa4932c1123a651e888694a
Dominik Fringeli <dfringeli@outlook.com>
2020-11-07 10:24:54 +0100

    added imports in ifc_data_checker/init.py

---
+++ ifc_data_checker/__init__.py
@@ -0,0 +1,11 @@
+"""init ifc_data_checker"""
+
+from ifc_data_checker import checker
+from ifc_data_checker import config
+from ifc_data_checker import constraint_checks
+from ifc_data_checker import constraints
+from ifc_data_checker import path_operators
+from ifc_data_checker import report
+from ifc_data_checker import rules
+from ifc_data_checker import validation
+from ifc_data_checker import yaml
